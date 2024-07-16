5008a7d902446725ef5ebb90d8482a9f55711530
Cristian Alcantar <cristron2014@hotmail.com>
2024-07-16 15:04:11 -0700

    [dev] algo

--- alienware_project/API/Controllers/WeatherForecastController.cs
+++ alienware_project/API/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Algo"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
@@ -20,15 +20,10 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public IActionResult Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return Ok(Summaries);
+            
     }
 }
 
