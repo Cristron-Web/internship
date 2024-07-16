7a142c1b6f7b42e8d56acbc055fd06593cfe8723
Cristian Alcantar <cristron2014@hotmail.com>
2024-07-16 15:17:27 -0700

    [dev] cambio

--- alienware_project/API/Controllers/WeatherForecastController.cs
+++ alienware_project/API/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Algo"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Cambio"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
